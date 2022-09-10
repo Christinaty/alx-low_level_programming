8dc2031c342666e6b8413d7b3451dd5859120ee2
Christina <111030842+Christinaty@users.noreply.github.com>
2022-09-10 02:35:52 +0100

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,20 @@
-lines (17 sloc)  242 Bytes
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
-* main - Prints the alphabet.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char low;
 
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
-	}
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
 	putchar('\n');
+
 	return (0);
 }
