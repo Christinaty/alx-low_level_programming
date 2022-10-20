4356595258a1f5081f165d5571312f69fbc6c657
Christina <111030842+Christinaty@users.noreply.github.com>
2022-10-20 04:58:10 +0100

    Update 4-add.c

--- 0x0A-agrc_argv/4-add.c
+++ 0x0A-agrc_argv/4-add.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /**
- * main - main funct
- * @argc: param count
- * @argv: param poiter
- * Return: int
- */
+* main - adds numbers
+* @argc: number of arguments passed to the function
+* @argv: argument vector of pointers to strings
+*
+* Return: 0 if no errors, else 1
+*/
+
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int a = 0, i, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (isdigit(argv[i][j]) == 0)
 			{
-				printf("Error\n");
+				puts("Error");
 				return (1);
 			}
 		}
-		sum += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
-
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
 	return (0);
 }
