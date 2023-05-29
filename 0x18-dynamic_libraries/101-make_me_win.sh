7cca40e9fc62240171e02a10e905cc5681024f16
Christina <111030842+Christinaty@users.noreply.github.com>
2023-05-29 17:11:18 +0100

    lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-wget -P /tmp/ https://raw.github.com/Christinaty/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-
+wget -P /tmp/ https://github.com/Christinaty/alx-low_level_programming.git
 export LD_PRELOAD=/tmp/nrandom.so
